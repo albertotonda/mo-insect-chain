8130e33e9a018ff3d7663c4ac4ad07d93183ee5c
albertotonda <alberto.tonda@gmail.com>
2022-01-24 12:04:51 +0100

    Minor modifications in the visualization script.

--- src/visualize-results.py
+++ src/visualize-results.py
@@ -7,16 +7,18 @@ import pandas as pd
 import seaborn as sns
 import sys
 
+from mpl_toolkits.mplot3d import Axes3D # this is for 3D figures
+
 sns.set_theme() # set the SeaBorn graphical theme, it looks nice 
 
-def plot_2d(df, fitness_a, fitness_b) :
+def plot_2d(df, output_folder, fitness_a, fitness_b) :
 
     # set the data
     fitness_a_values = df[fitness_a].values
     fitness_b_values = df[fitness_b].values
 
     # plot the figure
-    fig = plt.figure()
+    fig = plt.figure(figsize=(8,8))
     ax = fig.add_subplot(1,1,1)
 
     ax.scatter(fitness_a_values, fitness_b_values, color='blue', alpha=0.7) # alpha sets transparency
@@ -47,11 +49,26 @@ def main() :
     if not os.path.exists(output_folder) : os.makedirs(output_folder)
 
     # let's create some visualizations!
-    plot_2d(df, fitness_names[0], fitness_names[1])
-    plot_2d(df, fitness_names[1], fitness_names[2])
-    plot_2d(df, fitness_names[0], fitness_names[2])
+    plot_2d(df, output_folder, fitness_names[0], fitness_names[1])
+    plot_2d(df, output_folder, fitness_names[1], fitness_names[2])
+    plot_2d(df, output_folder, fitness_names[0], fitness_names[2])
 
-    # TODO also, a 3d plot with everything
+    # also, a 3d plot with everything
+    #fig = plt.figure(figsize=(8,8))
+    fig = plt.figure()
+    ax = Axes3D(fig)
+
+    x = df[fitness_names[0]].values
+    y = df[fitness_names[1]].values
+    z = df[fitness_names[2]].values
+
+    ax.scatter(x, y, z, c=x, marker='o', alpha=0.7)
+    ax.set_xlabel(fitness_names[0])
+    ax.set_ylabel(fitness_names[0])
+    ax.set_zlabel(fitness_names[0])
+
+    plt.savefig(os.path.join(output_folder, "3dplot.png"), dpi=300)
+    plt.show()
 
     return
 
